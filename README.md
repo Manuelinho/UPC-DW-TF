954d8c1f82b5f548ac7967688e281c4a1616e6f1
David Sanchez <dsanchezsalazar@gmail.com>
2013-05-19 18:25:11 -0500

    Creacion de la aplicacion web

---
+++ README.md
@@ -0,0 +1,5 @@
+"Trabajo Desarrollo Entorno Web" 
+
+U201220857 Vivas Baca Guillermo
+U201220621 Borda Anaya Manuel
+U201216844 Sanchez Salaza David
---
+++ concierto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concierto::Application.config.secret_token = '7a1e8f849cfe1984690709c8d19f978ce6d78f72f5e4908564de694ab0db96ac21864890553456ed38bba452d0b2e034d33d1805ed14ec410656dc402f91c320'
